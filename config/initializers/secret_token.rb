d0fe25d39ee41a1915948cc1d0cf476d57f54668
Dunya Kirkali <dunyakirkali@gmail.com>
2014-01-25 01:49:30 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Osizer::Application.config.secret_key_base = '54d1048bdbe8bf9583fbb078ff646e725302cd755183c33bc9b620b9c080c583d9b390d551c6ade3776fd23c2faa97cf3980cb89bfb738be2b83b2a1d685f5b1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Osizer::Application.config.session_store :cookie_store, key: '_osizer_session'
