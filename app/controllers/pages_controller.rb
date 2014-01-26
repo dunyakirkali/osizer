f957784a56ddc2185d1e8c4a28b41a9a0479ebd3
Dunya Kirkali <dunyakirkali@gmail.com>
2014-01-26 18:56:10 +0100

    removed pages controller

--- app/controllers/pages_controller.rb
+++
@@ -1,5 +0,0 @@
-class PagesController < ApplicationController
-  def home
-    @image = Image.new
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Osizer::Application.routes.draw do
   resources :images
 
-  get "pages/home"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-root 'pages#home'
+  root 'images#new'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
